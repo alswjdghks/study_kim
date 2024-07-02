4950c4d1c50ce06628342bcec16bc83aaaa6f39a
DESKTOP-24E7HA8\jerry <jerrymin1221@naver.com>
2024-07-02 23:56:47 +0900

    주문조회V5:JPA에서 DTO 직접 조회- 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -2,9 +2,12 @@ package jpabook.jpashop.repository.order.query;
 
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.mapping.Collection;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -27,6 +30,32 @@ public class OrderQueryRepository {
         });
         return result;
     }
+
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        //orderItem 컬렉션을 MAP 한방에 조회
+        Map<Long, List<OrderItemQueryDto>> orderItemMap =
+                findOrderItemMap(toOrderIds(result));
+        //루프를 돌면서 컬렉션 추가(추가 쿼리 실행X
+        result.forEach(o->o.setOrderItems(orderItemMap.get(o.getOrderId())));
+        return result;
+    }
+
+    private Map<Long, List<OrderItemQueryDto>> findOrderItemMap(List<Long> orderIds) {
+        List<OrderItemQueryDto> orderItems = em.createQuery("select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count) " +
+                        " from OrderItem oi" +
+                        " join oi.item i" +
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class)
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+        return orderItems.stream().collect(Collectors.groupingBy(OrderItemQueryDto::getOrderId));
+    }
+
+    private List<Long> toOrderIds(List<OrderQueryDto> result) {
+        return result.stream().map(o->o.getOrderId()).collect(Collectors.toList());
+    }
+
 
     /**
      * 1:N 관계(컬렉션)를 제외한 나머지를 한번에 조회
